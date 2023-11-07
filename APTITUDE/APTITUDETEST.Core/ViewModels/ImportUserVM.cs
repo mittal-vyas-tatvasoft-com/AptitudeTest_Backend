82738ded7c76ffd06c2be12d2dadc54c9f987ccd
harshtatva2002 <harsh.pateltraining@gmail.com>
2023-11-07 18:25:33 +0530

    2 changes

--- APTITUDE/APTITUDETEST.Core/ViewModels/ImportUserVM.cs
+++ APTITUDE/APTITUDETEST.Core/ViewModels/ImportUserVM.cs
@@ -1,11 +1,12 @@
 ﻿using AptitudeTest.Core.Validations;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace AptitudeTest.Core.ViewModels
 {
     public class ImportUserVM
     {
-        //[Required(ErrorMessage = "Please select a file.")]
+        [Required(ErrorMessage = "Please select a file.")]
         [FileExtensionMustBeValid(".txt", ".csv")] // Add the allowed extensions you want to allow
         public IFormFile file { get; set; }
         public int? GroupId { get; set; }
