499f779e29e445bbb2de8e296fae3a3fcafe1a8e
krishna-soni <krishna.soni@tatvasoft.com>
2023-11-07 12:25:03 +0530

    Added C# classes and Db Context configuration for DB tables TestQuestions and TestQuestionsCount

---
+++ APTITUDE/APTITUDETEST.Core/Entities/Test/TestQuestions.cs
@@ -0,0 +1,29 @@
+﻿
+using AptitudeTest.Core.Entities.Questions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AptitudeTest.Core.Entities.Test
+{
+    public class TestQuestions
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("Tests")]
+        public int TestId { get; set; }
+        [ForeignKey("QuestionModules")]
+        public int TopicId { get; set; }
+        public int NoOfQuestions { get; set; }
+        public int Weightage { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CreatedDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+        public virtual Test Tests { get; set; }
+        public virtual QuestionModule QuestionModules { get; set; }
+    }
+}
---
+++ APTITUDE/APTITUDETEST.Core/Entities/Test/TestQuestionsCount.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AptitudeTest.Core.Entities.Test
+{
+    public class TestQuestionsCount
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("TestQuestions")]
+        public int TestQuestionId { get; set; }
+        [Range(1, 2)]
+        public int QuestionType { get; set; }
+        public int OneMarks { get; set; }
+        public int TwoMarks { get; set; }
+        public int ThreeMarks { get; set; }
+        public int FourMarks { get; set; }
+        public int FiveMarks { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? CreatedDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+        public virtual TestQuestions TestQuestions { get; set; }
+    }
+}
