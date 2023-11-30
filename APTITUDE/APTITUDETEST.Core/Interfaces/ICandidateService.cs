7234576f4572225466d02df3d5df301cdb50c3f7
krishna-soni <krishna.soni@tatvasoft.com>
2023-11-30 14:43:20 +0530

    Added StartUserTest API

--- APTITUDE/APTITUDETEST.Core/Interfaces/ICandidateService.cs
+++ APTITUDE/APTITUDETEST.Core/Interfaces/ICandidateService.cs
@@ -7,7 +7,7 @@ namespace AptitudeTest.Core.Interfaces
     public interface ICandidateService
     {
         Task<JsonResult> CreateUserTest(CreateUserTestVM userTest);
-        Task<JsonResult> CreateTempUserTest(CreateTempUserTestVM tempUserTest);
+        Task<JsonResult> CreateTempUserTest(int userId);
         Task<JsonResult> CreateUserTestResult(CreateUserTestResultVM userTestResult);
         Task<JsonResult> CreateTempUserTestResult(CreateUserTestResultVM tempUserTestResult);
     }
