941d5032c262c3efddb102cb8fc2a83081376094
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2023-12-11 15:47:56 +0530

    Result API Folder Structure

---
+++ APTITUDE/APTITUDETEST.Application/Services/ResultService.cs
@@ -0,0 +1,26 @@
+﻿using AptitudeTest.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AptitudeTest.Application.Services
+{
+    public class ResultService : IResultService
+    {
+        #region Properties
+        private readonly IResultRepository _repository;
+        #endregion
+
+        #region Constructor
+        public ResultService(IResultRepository repository)
+        {
+            _repository = repository;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<JsonResult> Get(int id, int marks, int pageSize, int pageIndex)
+        {
+            return await _repository.Get(id, marks, pageSize, pageIndex);
+        }
+        #endregion
+    }
+}
---
+++ APTITUDE/AptitudeTest.API/Controllers/ResultsController.cs
@@ -0,0 +1,39 @@
+﻿using AptitudeTest.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AptitudeTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ResultsController : ControllerBase
+    {
+        #region Properties
+        private readonly IResultService _service;
+        #endregion
+
+        #region Constructor
+        public ResultsController(IResultService service)
+        {
+            _service = service;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// It gives User Result
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="marks"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageIndex"></param>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public async Task<JsonResult> Get(int id, int marks, int pageSize, int pageIndex)
+        {
+            return await _service.Get(id, marks, pageSize, pageIndex);
+        }
+        #endregion
+    }
+}
