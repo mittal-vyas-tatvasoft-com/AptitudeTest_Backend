107e0f910415fe4cfbb3718d943312bf2f54e4c1
Divyapalsinh Vala <divyapalsinh@aramex.com>
2023-12-20 15:38:35 +0530

    Added xid in swagger doc

---
+++ APTITUDE/AptitudeTest.API/Filters/AddXIdSwagggerFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AptitudeTest.Filters
+{
+    public class AddXIdSwagggerFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = "Xid",
+                Description = "Latest Session Id",
+                In = ParameterLocation.Header,
+                Schema = new OpenApiSchema() { Type = "String" },
+                Required = false,
+                Example = new OpenApiString("")
+            });
+        }
+    }
+}
