36d2736007f652e3e7b4f324af346be81fed1679
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2023-12-26 10:53:39 +0530

    Folder Structure for Reports Controller

---
+++ APTITUDE/APTITUDETEST.Application/Services/ReportsService.cs
@@ -0,0 +1,21 @@
+﻿using AptitudeTest.Core.Interfaces;
+
+namespace AptitudeTest.Application.Services
+{
+    public class ReportsService : IReportsService
+    {
+        #region Properties
+        private readonly IReportsRepository _repository;
+        #endregion
+
+        #region Constructor
+        public ReportsService(IReportsRepository repository)
+        {
+            _repository = repository;
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ APTITUDE/AptitudeTest.API/Controllers/ReportsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AptitudeTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+    }
+}
