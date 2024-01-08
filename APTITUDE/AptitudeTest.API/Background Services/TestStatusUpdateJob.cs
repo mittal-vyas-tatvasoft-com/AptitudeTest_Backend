1adb9754b5371010ef21316529cb991020f3da71
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2024-01-08 11:43:12 +0530

    Job Added for Automatically Updating Test Status

---
+++ APTITUDE/AptitudeTest.API/Background Services/TestStatusUpdateJob.cs
@@ -0,0 +1,98 @@
+﻿using AptitudeTest.Core.Entities.Test;
+using AptitudeTest.Core.Interfaces;
+using APTITUDETEST.Common.Data;
+using Microsoft.EntityFrameworkCore;
+using static AptitudeTest.Data.Common.Enums;
+
+namespace AptitudeTest.Background_Services
+{
+    public class TestStatusUpdateJob : BackgroundService
+    {
+        #region Properties
+        private AppDbContext _context;
+        private readonly ILoggerManager _logger;
+        private Timer? _timer;
+        int jobRunTime;
+        int extraTime;
+        #endregion
+
+        #region Constructor
+        public TestStatusUpdateJob(ILoggerManager logger, IConfiguration config)
+        {
+            _context = new AppDbContext(new DbContextOptions<AppDbContext>());
+            _logger = logger;
+            IConfiguration _config;
+            _config = config;
+            jobRunTime = Int32.Parse(_config["TestStatusUpdate:JobRunTime"]);
+            extraTime = Int32.Parse(_config["TestStatusUpdate:TimeBeforeStartTimetoUpdateStatus"]);
+            SetMainTimer();
+        }
+        #endregion
+
+        #region Methods
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void SetMainTimer()
+        {
+            DateTime now = DateTime.Now;
+            DateTime scheduledTime = new DateTime(now.Year, now.Month, now.Day, jobRunTime, 0, 0);
+            if (now > scheduledTime)
+            {
+                scheduledTime = scheduledTime.AddDays(1);
+            }
+            TimeSpan timeUntilScheduled = scheduledTime - now;
+            _timer = new Timer(x => CalculateTime(), null, (int)timeUntilScheduled.TotalMilliseconds, 24 * 60 * 60 * 1000);
+        }
+
+        private void CalculateTime()
+        {
+            try
+            {
+                DateTime today = DateTime.Today;
+                List<DateTime> dateTimes = new List<DateTime>();
+                List<Test> tests = _context.Tests.Where(t => t.StartTime.Date == today.Date).ToList();
+                if (tests != null)
+                {
+                    foreach (var test in tests)
+                    {
+                        Task.Delay((int)CalculateDelay(test.StartTime)).ContinueWith(task => UpdateStatus(test.Id));
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error occurred in TestStatusUpdateJob.CalculateTime:{ex} at: {DateTime.Now}");
+            }
+
+        }
+
+        private void UpdateStatus(int testId)
+        {
+            try
+            {
+                Test test = _context.Tests.Where(t => t.Id == testId).FirstOrDefault();
+                test.Status = (int)TestStatus.Active;
+                _context.Update(test);
+                _context.SaveChanges();
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error occurred in TestStatusUpdateJob.UpdateStatus:{ex} at: {DateTime.Now}");
+            }
+
+        }
+
+        private double CalculateDelay(DateTime dateTime)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan timeSpan = dateTime - now;
+            return timeSpan.TotalMilliseconds - extraTime * 60 * 1000;
+        }
+        #endregion
+    }
+}
