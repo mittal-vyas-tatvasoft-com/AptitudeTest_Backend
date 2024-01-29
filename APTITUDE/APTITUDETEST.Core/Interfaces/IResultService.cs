49c446363e0f5cd5b1229b94d14e16625557a6e8
Meetkumar-Tatva <meet.thakkar@tatvamail.com>
2024-01-29 15:32:21 +0530

    Filter added in Result Details Backend

--- APTITUDE/APTITUDETEST.Core/Interfaces/IResultService.cs
+++ APTITUDE/APTITUDETEST.Core/Interfaces/IResultService.cs
@@ -5,7 +5,7 @@ namespace AptitudeTest.Core.Interfaces
 {
     public interface IResultService
     {
-        public Task<JsonResult> Get(int id, int testId, int marks, int pageSize, int pageIndex);
+        public Task<JsonResult> Get(int id, int testId, int marks, int pageSize, int pageIndex, bool onlyCorrect);
         public Task<JsonResult> GetResults(string? searchQuery, int? TestId, int? GroupId, int? CollegeId, int? Year, int? currentPageIndex, int? pageSize, string? sortField, string? sortOrder);
         public Task<JsonResult> GetResultStatistics(string? searchQuery, int? TestId, int? GroupId, int? CollegeId, int? Year, int? currentPageIndex, string? sortField, string? sortOrder);
         public Task<JsonResult> GetResultExportData(string? searchQuery, int? TestId, int? GroupId, int? CollegeId, int? Year, int? currentPageIndex, int? pageSize, string? sortField, string? sortOrder);
