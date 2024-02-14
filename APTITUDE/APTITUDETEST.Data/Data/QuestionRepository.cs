1d810374fd4dfebbb6d56a7a313a3c7764944bca
mittal.vyas@tatvasoft.com <mittal.vyas@tatvasoft.com>
2024-02-14 07:49:13 -0500

    Question Count updated

--- APTITUDE/APTITUDETEST.Data/Data/QuestionRepository.cs
+++ APTITUDE/APTITUDETEST.Data/Data/QuestionRepository.cs
@@ -9,12 +9,14 @@ using CsvHelper.Configuration;
 using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Globalization;
 using System.Text;
 using System.Web;
 using static AptitudeTest.Data.Common.Enums;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AptitudeTest.Data.Data
 {
@@ -225,19 +227,13 @@ namespace AptitudeTest.Data.Data
             try
             {
                 QuestionCountVM questionCount = new QuestionCountVM();
-                if (status == null)
+                using (DbConnection connection = new DbConnection())
                 {
-                    questionCount.TotalCount = _context.Questions.Where(q => q.IsDeleted != true).Count();
-                    questionCount.MathsCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Maths).Count();
-                    questionCount.ReasoningCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Reasoning).Count();
-                    questionCount.TechnicalCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Technical).Count();
-                }
-                else
-                {
-                    questionCount.TotalCount = _context.Questions.Where(q => q.IsDeleted != true && q.Status == status).Count();
-                    questionCount.MathsCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Maths && q.Status == status).Count();
-                    questionCount.ReasoningCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Reasoning && q.Status == status).Count();
-                    questionCount.TechnicalCount = _context.Questions.Where(q => q.IsDeleted != true && q.Topic == (int)Enums.QuestionTopic.Technical && q.Status == status).Count();
+                    List<TestTopicWiseCountVM> data = connection.Connection.Query<TestTopicWiseCountVM>("Select * from gettopicwisequestionscountforquestionmodule(@filterStatus)", new { filterStatus = status }).ToList();
+                    questionCount.TotalCount = data.Sum(x =>x.CountOfQuestions);
+                    questionCount.MathsCount = data.Where(x => x.TopicId == (int)QuestionTopic.Maths).Select(x=>x.CountOfQuestions).FirstOrDefault();
+                    questionCount.ReasoningCount = data.Where(x=>x.TopicId==(int)QuestionTopic.Reasoning).Select(x => x.CountOfQuestions).FirstOrDefault();
+                    questionCount.TechnicalCount = data.Where(x => x.TopicId == (int)QuestionTopic.Technical).Select(x => x.CountOfQuestions).FirstOrDefault();
                 }
                 return new JsonResult(new ApiResponse<QuestionCountVM>
                 {
