f070bb42f7e0482d3280712831b16c868c24286a
mittal.vyas@tatvasoft.com <mittal.vyas@tatvasoft.com>
2024-04-15 14:57:53 +0530

    Index added in results data

--- APTITUDE/APTITUDETEST.Core/ViewModels/ResultsVM.cs
+++ APTITUDE/APTITUDETEST.Core/ViewModels/ResultsVM.cs
@@ -3,6 +3,7 @@
     public class ResultsVM
     {
         public int TimeRemaining { get; set; }
+        public int Index { get; set; }
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string FatherName { get; set; }
